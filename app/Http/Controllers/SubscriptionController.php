b2db3a7b9b24e6d6233467e73ae749089032b229
Olalekan ISMAIL <irolalekan@gmail.com>
2021-04-17 23:25:07 +0100

    create subbscription Resource, Controller, and Service

---
+++ app/Http/Controllers/SubscriptionController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Subscription;
+use Illuminate\Http\Request;
+use App\Events\NewTopicPublished;
+use App\Services\SubscriptionService;
+use App\Http\Resources\CreateSubscriptionResource;
+
+class SubscriptionController extends Controller
+{
+    protected $ss;
+
+    public function __construct(SubscriptionService $ss)
+    {
+        $this->ss = $ss;
+    }
+    
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function subscribeToTopic(Request $request, $topic)
+    {
+        $request->validate([
+            "url" => ["required", "url"]
+        ]);
+
+        $sub = $this->ss->createSubscription($request->url, $topic);
+        
+        if($sub["success"]) return new CreateSubscriptionResource($sub["data"]);
+
+        return response()->json($sub, 400);
+    }
+
+    public function publishSubscription(Request $request, $topic)
+    {
+        $subs = $this->ss->getByTopic($topic);
+
+        if($subs->count() > 0) {
+            event(new NewTopicPublished($subs, $request->all()));
+
+            // return response to user
+        }
+        // if no $subs then notify the user or perform other action(s)
+        
+    }
+}
---
+++ app/Services/SubscriptionService.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Subscription;
+use App\Models\Topic;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
+
+class SubscriptionService
+{
+
+    /**
+     *  Creates a new subscription
+     * 
+     * @param string $url
+     * @param string $topic
+     * 
+     * @return array $response
+     */
+    public function createSubscription(string $url, string $topic)
+    {
+        try {
+            $sub = new Subscription;
+
+            $sub->url = $url;
+            $sub->topic = $topic;
+            $sub->save();
+
+            return ["data" => $sub, "message" => "subscription created", "success" => true];
+
+        } catch (\Throwable $th) {
+            Log::error("unable to create subscription", ["error" => $th->getMessage()]);
+            
+            return ["error" => "unable to create subscription", "data" =>  $th->getMessage(), "success" => false];
+        }
+    }
+
+    /**
+     * Find all subscription with the passed topic
+     * 
+     * @param string $topic
+     * 
+     * @return collection
+     */
+    public function getByTopic($topic)
+    {
+        $sub = Subscription::select(["url", "topic"])->where(["topic" => $topic])->get();
+
+        return $sub;
+
+    }
+
+    /**
+     * Notify the pass subscriber the given payload
+     * 
+     * @param \App\Models\Subscription $sub
+     * @param object|array $payload
+     * 
+     * 
+     */
+    public function notify($sub, $payload)
+    {
+        try {
+            $v = Http::post($sub->url, $payload);
+            Log::error("unable to notify subscriber", ["error" => $v]);
+        } catch (\Throwable $th) {
+            Log::error("unable to notify subscriber", ["error" => $th->getMessage()]);
+        }
+    }
+}
